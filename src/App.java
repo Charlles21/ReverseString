7115712f396af4c67d75a78710205220fc53d23b
Charlles <matheuscharles453@gmail.com>
2022-06-29 17:47:17 -0300

    master

---
+++ src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String nome = "Socorram-me! Subi no ônibus em Marrocos";
+
+        nome.substring(1);
+        System.out.println(reverse(nome));  
+        
+        
+        
+    }
+
+    public static String reverse(String nome){
+        String teste ="";
+        for (int i = nome.length() -1 ; i >= 0; i--) {             
+            
+            teste = teste + nome.charAt(i);
+            
+        
+        }
+
+
+
+        return teste;
+    }
+}
